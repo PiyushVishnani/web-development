031fbb83e0b8530971103af51983a2f1eb342392
piyush <vishnanipiyush@gmail.com>
2023-06-05 14:44:37 +0530

    Functions in JavaScript

---
+++ tut53.js
@@ -0,0 +1,18 @@
+console.log("This is tutorial 53");
+
+let name1 = "Piyush";
+let name2 = "Vishvajeet";
+let name3 = "Aditya";
+let name4 = "Robert";
+
+console.log(name1 + " is a good boy.");
+console.log(name2 + " is a good boy.");
+console.log(name3 + " is a good boy.");
+console.log(name4 + " is a good boy.");
+
+// or this can be done by creating a funtion
+
+function greet(name) {
+    console.log(name + " is a good boy.");
+}
+greet("Piyush Vishnani ");
