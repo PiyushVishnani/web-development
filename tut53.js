75aed00aeeaa002023051f50571e2555abe8d0db
piyush <vishnanipiyush@gmail.com>
2023-06-07 20:04:33 +0530

    some changes in functions

--- tut53.js
+++ tut53.js
@@ -13,6 +13,13 @@ console.log(name4 + " is a good boy.");
 // or this can be done by creating a funtion
 
 function greet(name) {
-    console.log(name + " is a good boy.");
+    console.log("Good morning "+ name);
 }
 greet("Piyush Vishnani ");
+
+function sum(a,b,c) {
+    let d = a+b+c;
+    return d;
+    console.log("this line is not reachable because this is after return");
+}
+console.log(sum(2,3,5));
