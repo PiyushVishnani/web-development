5f60d269f6078116dfb2eecb128094535e3bcc34
piyush <vishnanipiyush@gmail.com>
2023-06-26 11:13:21 +0530

    variable

---
+++ JavaScript/index.js
@@ -0,0 +1,15 @@
+let x = 'Piyush';
+console.log(x);
+console.log(typeof x); 
+
+let y = true;
+console.log(y);
+console.log(typeof y);
+
+let z = null;
+console.log(z);
+console.log(typeof z);
+
+let v;
+console.log(v);
+console.log(typeof v);
